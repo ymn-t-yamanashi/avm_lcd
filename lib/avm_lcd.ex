e024f3960c205eca182c4338a3408f0e3bd722bc
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-11-29 17:32:45 +0900

    バックライトのみ成功

--- lib/avm_lcd.ex
+++ lib/avm_lcd.ex
@@ -19,23 +19,38 @@ defmodule AvmLcd do
   @reg_output 0x08
 
   def start do
-    # --- 修正箇所: XIAO ESP32S3 のピン番号 (SDA=5, SCL=6) ---
-    {:ok, i2c} = I2C.open(sda: 5, scl: 6, speed_hz: 100_000)
-    # -----------------------------------------------------
+    Process.sleep(3000)
+    IO.inspect("YMN test")
 
-    # 初期化
+    # # --- 修正箇所: XIAO ESP32S3 のピン番号 (SDA=5, SCL=6) ---
+    # {_, _i2c} =
+    i2c = I2C.open([{:scl, 6}, {:sda, 5}, {:clock_speed_hz, 1_000_000}])
+    # # -----------------------------------------------------
+
+    IO.inspect("OK")
+    # # 初期化
     init_lcd(i2c)
+
     init_rgb(i2c)
+    IO.inspect("OK2-1")
 
-    # 色をセット (青色にしてみましょう)
-    set_rgb(i2c, 0, 0, 255)
+    loop(i2c, 100)
 
-    # 文字を表示
-    print(i2c, "Hello XIAO!")
+    # # 文字を表示
+    # print(i2c, "Hello XIAO!")
 
-    # 2行目へ移動して表示する例（コマンド 0xC0 = 2行目先頭）
-    send_cmd(i2c, 0xC0)
-    print(i2c, "ExAtomVM Rocks")
+    # # 2行目へ移動して表示する例（コマンド 0xC0 = 2行目先頭）
+    # send_cmd(i2c, 0xC0)
+    # print(i2c, "ExAtomVM Rocks")
+  end
+
+  def loop(i2c, 255), do: loop(i2c, 0)
+
+  def loop(i2c, c) do
+    #Process.sleep(100)
+    IO.inspect(c)
+    set_rgb(i2c, 255, 0, c)
+    loop(i2c, c + 1)
   end
 
   # --- 以下は変更なし ---
@@ -59,24 +74,24 @@ defmodule AvmLcd do
 
   @compile {:no_warn_undefined, [I2C]}
   defp send_cmd(i2c, cmd) do
-    I2C.write(i2c, @lcd_addr, <<@lcd_cmd_mode, cmd>>)
+    I2C.write_bytes(i2c, @lcd_addr, <<@lcd_cmd_mode, cmd>>)
   end
 
   @compile {:no_warn_undefined, [I2C]}
   defp send_data(i2c, data) do
-    I2C.write(i2c, @lcd_addr, <<@lcd_data_mode, data>>)
+    I2C.write_bytes(i2c, @lcd_addr, <<@lcd_data_mode, data>>)
   end
 
   @compile {:no_warn_undefined, [I2C]}
   def init_rgb(i2c) do
-    I2C.write(i2c, @rgb_addr, <<@reg_mode1, 0x00>>)
-    I2C.write(i2c, @rgb_addr, <<@reg_output, 0xAA>>)
+    I2C.write_bytes(i2c, @rgb_addr, <<@reg_mode1, 0x00>>)
+    I2C.write_bytes(i2c, @rgb_addr, <<@reg_output, 0xAA>>)
   end
 
   @compile {:no_warn_undefined, [I2C]}
   def set_rgb(i2c, r, g, b) do
-    I2C.write(i2c, @rgb_addr, <<@reg_red, r>>)
-    I2C.write(i2c, @rgb_addr, <<@reg_green, g>>)
-    I2C.write(i2c, @rgb_addr, <<@reg_blue, b>>)
+    I2C.write_bytes(i2c, @rgb_addr, <<@reg_red, r>>)
+    I2C.write_bytes(i2c, @rgb_addr, <<@reg_green, g>>)
+    I2C.write_bytes(i2c, @rgb_addr, <<@reg_blue, b>>)
   end
 end
