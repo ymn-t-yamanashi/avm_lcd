0b52c76b47a02b010e3cd24ae92f07cac86e937a
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-11-29 18:33:42 +0900

    文字解決

--- lib/avm_lcd.ex
+++ lib/avm_lcd.ex
@@ -23,9 +23,7 @@ defmodule AvmLcd do
     IO.inspect("YMN test")
 
     # # --- 修正箇所: XIAO ESP32S3 のピン番号 (SDA=5, SCL=6) ---
-    # {_, _i2c} =
     i2c = I2C.open([{:scl, 6}, {:sda, 5}, {:clock_speed_hz, 1_000_000}])
-    # # -----------------------------------------------------
 
     IO.inspect("OK")
     # # 初期化
@@ -34,42 +32,44 @@ defmodule AvmLcd do
     init_rgb(i2c)
     IO.inspect("OK2-1")
 
-    loop(i2c, 100)
+    set_rgb(i2c, 255, 255, 255)
 
-    # # 文字を表示
-    # print(i2c, "Hello XIAO!")
-
-    # # 2行目へ移動して表示する例（コマンド 0xC0 = 2行目先頭）
-    # send_cmd(i2c, 0xC0)
-    # print(i2c, "ExAtomVM Rocks")
-  end
-
-  def loop(i2c, 255), do: loop(i2c, 0)
-
-  def loop(i2c, c) do
-    #Process.sleep(100)
-    IO.inspect(c)
-    set_rgb(i2c, 255, 0, c)
-    loop(i2c, c + 1)
+    # 文字を表示
+    print(i2c, "YMN Test")
   end
 
   # --- 以下は変更なし ---
-
   def init_lcd(i2c) do
-    Process.sleep(50)
+    # 1. Function Set (0x38: DL=1: 8bit, N=1: 2R, F=0: 5x7)
+    #    - AIP31068Lの Function Set コマンドは 0b0011NFXX, N=1, F=0 の場合 0b001110XX = 0x38
     send_cmd(i2c, 0x38)
-    Process.sleep(5)
-    send_cmd(i2c, 0x38)
+    # Wait for more than 40us (1msで十分)
     Process.sleep(1)
+
+    # 2. Display ON/OFF Control (0x0C: D=1, C=0, B=0)
     send_cmd(i2c, 0x0C)
+    # Wait for more than 39us (1msで十分)
+    Process.sleep(1)
+
+    # 3. Display Clear (0x01)
     send_cmd(i2c, 0x01)
-    Process.sleep(5)
+    # Wait for more than 1.53ms (2msで十分)
+    Process.sleep(2)
+
+    # 4. Entry Mode Set (0x06: I/D=1, S=0)
     send_cmd(i2c, 0x06)
+    # 念のため
+    Process.sleep(1)
   end
 
-  def print(i2c, str) do
-    String.to_charlist(str)
-    |> Enum.each(fn char -> send_data(i2c, char) end)
+  # 代替案: パターンマッチングで1バイトずつ取り出す
+  def print(i2c, <<char, rest::binary>>) do
+    send_data(i2c, char)
+    print(i2c, rest)
+  end
+
+  def print(_i2c, "") do
+    :ok
   end
 
   @compile {:no_warn_undefined, [I2C]}
